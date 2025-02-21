ce5f1118ca5026a4e0a9fa3645d50e8d8d2df3b6
Ashley Davis Kosasih <ashleydaviskosasih@gmail.com>
2025-02-21 16:39:34 -0600

    initial commit

---
+++ devhack2025/server/app.js
@@ -0,0 +1,27 @@
+var express = require("express"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  cons = require("consolidate"),
+  dust = require("dustjs-helpers"),
+  pg = require("pg"),
+  app = express();
+
+var connect = "postgres://postgres:123456789@localhost/devhackdb"; //DB connect string
+
+app.engine("dust", cons.dust);
+
+app.set("view enigne", "dust");
+app.set("views", __dirname + "/views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  console.log("TEST");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on Port 3000");
+});
