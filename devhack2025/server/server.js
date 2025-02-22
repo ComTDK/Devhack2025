5c01f4fff284de8c6d7028df584761fb43446d7c
Ashley Davis Kosasih <ashleydaviskosasih@gmail.com>
2025-02-21 23:12:42 -0600

    trying to set up express API to interact with postgreSQL

--- devhack2025/server/server.js
+++ devhack2025/server/server.js
@@ -1,3 +1,5 @@
+//------------------CREATE THE SERVER---------------------
+
 var express = require("express"),
   path = require("path"),
   bodyParser = require("body-parser"),
@@ -7,6 +9,8 @@ var express = require("express"),
   app = express();
 
 var connect = "postgres://postgres:123456789@localhost/devhackdb"; //DB connect string
+
+const PORT = 3000;
 
 app.engine("dust", cons.dust);
 
@@ -22,6 +26,27 @@ app.get("/", function (req, res) {
   console.log("TEST");
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log("Server started on Port 3000");
 });
+
+//--------------SETTING UP EXPRESS API TO INTERACT WITH POSTGRESQL----------------
+
+const express = require("express");
+const pool = require("./db"); // Import database connection
+
+const app = express();
+app.use(express.json()); // Middleware for parsing JSON
+
+// Route to get all users
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users"); // SQL query
+    res.json(result.rows); // Send result as JSON
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
